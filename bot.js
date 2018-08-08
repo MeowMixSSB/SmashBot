91860440993a82dc5792bfd8e0dc9903ba3fd296
MeowMixSSB <xconnorrose@gmail.com>
2018-08-08 06:55:21 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -6,7 +6,11 @@
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
-.listen(process.env.PORT || 5000)
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 
 if(!process.send) {
 
